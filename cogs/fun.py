86ec5f1e65bd2c2bf8dc4b1f47a760dfbe743a6e
Christopher Sien <imchrissien@gmail.com>
2020-08-28 16:56:28 -0700

    accounted for i don't know answers

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,18 +1,36 @@
 import discord
+import random
 from discord.ext import commands
 
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    idk = [
+        'who',
+        'what',
+        'when',
+        'where',
+        'why',
+        'how',
+        'wat',
+        'wot',
+        'wut',
+        'y '
+    ]
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.content.endswith('?'):
-            await message.channel.send('Yes')
+            if (' or ' in message.content.lower() or
+                    any(message.content.lower().startswith(starter) for starter in idk)):
+                await message.channel.send("I don't know")
+                return
+            await message.channel.send(random.choice(['Yes', 'No']))
 
     @commands.command()
     async def coinflip(self, ctx):
-        await ctx.send(f'Pong! {round(self.bot.latency * 1000)}ms')
+        await ctx.send(random.choice(['Heads!', 'Tails!']))
 
 def setup(bot):
     bot.add_cog(Fun(bot))
